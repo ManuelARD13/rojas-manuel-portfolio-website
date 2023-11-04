a62a063262c86b18f1f80ce79c8ac1d64701d8fe
swlman <duranalejandro661@gmail.com>
2023-11-04 09:42:10 -0300

    [STYLES] Completing basic blurbSection styles.

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -12,53 +12,53 @@ function MainContent() {
     setIndex(selectedIndex);
   };
 
-  const technologies = [
-    {
-      title: 'React.js',
-      img: 'img/react-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      link: '',
-    },
+  const lenguagesAndFrameworks = [
     {
       title: 'Next.js',
       img: 'img/nextjs-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'React framework for efficient server-side rendering and easy deployment.',
+      link: '',
+    },
+    {
+      title: 'React.js',
+      img: 'img/react-logo.png',
+      description: 'A powerful JavaScript library for building dynamic user interfaces',
       link: '',
     },
     {
       title: 'Bootstrap 5',
       img: 'img/bootstrap-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Leading framework for crafting sleek and quick responsive UIs',
       link: '',
     },
     {
       title: 'GitHub',
       img: 'img/github-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Top platform and essential hub for collaborative code management.',
       link: '',
     },
     {
-      title: 'HTML5',
+      title: 'HTML 5',
       img: 'img/html5-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Markup language for structuring web content and building web pages.',
       link: '',
     },
     {
-      title: 'CSS3',
+      title: 'CSS 3',
       img: 'img/css3-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Sheet style language for web styling, element design and layout.',
       link: '',
     },
     {
       title: 'JavaScript',
       img: 'img/js-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Dynamic scripting language able to create interactive user experiences',
       link: '',
     },
     {
       title: 'Git',
       img: 'img/git-logo.png',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Leading Version Control System for effective code management.',
       link: '',
     },
   ];
@@ -66,25 +66,25 @@ function MainContent() {
   const projects = [
     {
       title: 'Videogame DEMO',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Example of the character creation process in RPG games build in React.js',
       img: 'img/VideoGameDemo.png',
       link: '#',
     },
     {
       title: 'TODO App',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Implementing CRUD operations in React.js',
       img: 'img/TODOApp.png',
       link: '#',
     },
     {
       title: 'Weather App',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Implementing API consumption and data manipulation in React.js',
       img: 'img/VideoGameDemo.png',
       link: '#',
     },
     {
       title: 'Portfolio',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Front-end developer portfolio responsive website',
       img: 'img/portfolio-website.png',
       link: '#',
     },
@@ -92,22 +92,24 @@ function MainContent() {
 
   return (
     <>
-      <section className="d-flex flex-column justify-content-center align-items-center">
-        <h2>Technologies</h2>
-        <div className={`container-fluid d-flex flex-wrap justify-content-center gap-5 ${styles.cardsContainer}`}>
-          {technologies.map((technology) => (
+      <section className={`${styles.blurbsSection} d-flex flex-column justify-content-center align-items-center`}>
+        <h2 className='mt-5'>Regularly Implemented Lenguages and Frameworks</h2>
+        <p className={`${styles.divider} m-3`}></p>
+        <div className={`container-fluid d-flex flex-wrap justify-content-center gap-5 m-4 ${styles.cardsContainer}`}>
+          {lenguagesAndFrameworks.map((tech) => (
             <Card className={styles.card}>
-              <Card.Img variant="top" src={technology.img} />
+              <Card.Img variant="top" src={tech.img} />
               <Card.Body>
-                <Card.Title>{technology.title}</Card.Title>
-                <Card.Text>{technology.description}</Card.Text>
-                <Button href={technology.link} variant="primary">
-                  Know more
+                <Card.Title>{tech.title}</Card.Title>
+                <Card.Text>{tech.description}</Card.Text>
+                <Button href={tech.link} variant="primary">
+                  My Experience
                 </Button>
               </Card.Body>
             </Card>
           ))}
         </div>
+        <div className={styles.backgroundSquare}></div>
       </section>
       <section className="d-flex flex-column justify-content-center align-items-center">
         <h2>Professional Developement Projects</h2>
@@ -119,25 +121,10 @@ function MainContent() {
                 <img className={styles.slideImgs} src={project.img} alt="" />
                 <Carousel.Caption className={styles.carouselCaption}>
                   <h3>{project.title}</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  <p>{project.description}</p>
                 </Carousel.Caption>
               </Carousel.Item>
             ))}
-
-            {/* <Carousel.Item>
-              <img className={styles.slideImgs} src="img/svtJbZs.jpg" alt="" />
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img className={styles.slideImgs} src="img/svtJbZs.jpg" alt="" />
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </Carousel.Caption>
-            </Carousel.Item> */}
           </Carousel>
         </div>
       </section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,31 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import styles from "styles/Navbar.module.css";
+import styles from 'styles/Navbar.module.css';
 
 function Navigation() {
   return (
-    <Navbar expand="lg" className={`${styles.navbar} bg-body-tertiary`} data-bs-theme="dark" sticky='top'>
-      <Container className={styles.navContainer}>
+    <Navbar expand="lg" className={`${styles.navbar} bg-body-tertiary`} data-bs-theme="dark" sticky="top">
+      <Container>
         <Navbar.Brand href="#home">ROJAS Manuel</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse id="basic-navbar-nav" className={`${styles.navContainer}`}>
+          <Nav className={`me-auto`}>
             <Nav.Link href="#home">Home</Nav.Link>
             <NavDropdown title="Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                React.js Videogame Demo
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                React.js TODO App
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">
-                Next.js Weather App
-              </NavDropdown.Item>
-              {/* <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item> */}
+              <NavDropdown.Item href="#action/3.1">React.js Videogame Demo</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">React.js TODO App</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Next.js Weather App</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#link">About Me</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
@@ -35,6 +25,8 @@ function Navigation() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+ 
+    
   );
 }
 
